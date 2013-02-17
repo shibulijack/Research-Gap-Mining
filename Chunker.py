832cb569f446eb1b337251c9f103bc5a7d0437d4
sundar <sundarrajamanickam1992@gmail.com>
2013-02-17 15:46:43 +0530

    Updated text preprocessor

--- Chunker.py
+++ Chunker.py
@@ -2,38 +2,53 @@
 
 import nltk
 import string
+import enchant
 						
 def chunk(str):
 	tl2 = [];
+	d = enchant.Dict("en_US");
 	str = str.replace("'","");
+	str = str.replace("- ","");
+	str = str.replace("1)","p");
+	str = str.replace("l)","p");
 	sent_tokenizer=nltk.data.load('tokenizers/punkt/english.pickle');
 	sentences = sent_tokenizer.tokenize(str);		#Sentence Tokenization
 	for i in range(len(sentences)):
-		tokens = nltk.word_tokenize(sentences[i]);		#Word Tokenization		
-		tagged = nltk.pos_tag(tokens);
-		patterns = """NP:{<NN>+}
-						 {<NNP>+}
-		""";
-		NPChunker = nltk.RegexpParser(patterns);
-		result = NPChunker.parse(tagged);
-		tl1 = result.pos();								#Parse Tree Traversal
-		#Forming the vocab
-		t = '';
-		for j in range(len(tl1)):
-			if tl1[j][1] == 'NP':
-				t = t + tl1[j][0][0] + ' ';
-			else:
-				if t == '':
-					t = t + tl1[j][0][0];
-					tl2.append(t);
+		tokens1 = [];
+		tokens2 = [];
+		tokens = nltk.word_tokenize(sentences[i]);			#Word Tokenization	
+		tokens = [''.join(c for c in s if c not in string.punctuation) for s in tokens];		#Stripping special characters
+		tokens = [s for s in tokens if s];
+		for k in range(len(tokens)):
+			tokens2.append(filter(lambda s:s in string.printable,tokens[k]));
+		tokens2 = filter(None, tokens2);
+		for k in range(len(tokens2)):
+			if d.check(tokens2[k]) == True:
+				tokens1.append(filter(lambda s:s in string.printable,tokens2[k]));
+		tokens1 = filter(None, tokens1);
+		if len(tokens1) > 0:
+			tagged = nltk.pos_tag(tokens1);
+			patterns = """NP:{<NN>+}
+							{<NNP>+}
+			""";
+			NPChunker = nltk.RegexpParser(patterns);
+			result = NPChunker.parse(tagged);
+			tl1 = result.pos();								#Parse Tree Traversal
+			#Forming the vocab
+			t = '';
+			for j in range(len(tl1)):
+				if tl1[j][1] == 'NP':
+					t = t + tl1[j][0][0] + ' ';
 				else:
-					t = t[0:len(t)-1];
-					tl2.append(t);
-					tl2.append(tl1[j][0][0]);
-				t = '';
-		if t != '':
-			t = t[0:len(t)-1];
-			tl2.append(t);
-	tl2 = [''.join(c for c in s if c not in string.punctuation) for s in tl2];		#Stripping special characters
-	tl2 = [s for s in tl2 if s];
+					if t == '':
+						t = t + tl1[j][0][0];
+						tl2.append(t);
+					else:
+						t = t[0:len(t)-1];
+						tl2.append(t);
+						tl2.append(tl1[j][0][0]);
+					t = '';
+			if t != '':
+				t = t[0:len(t)-1];
+				tl2.append(t);
 	return tl2;
--- TextPreprocessor.py
+++ TextPreprocessor.py
@@ -2,43 +2,49 @@
 
 import nltk
 import string
+import os
 import re
+from nltk.stem.lancaster import LancasterStemmer
 
 import Chunker
-import PorterStemmer
 import SWRemover
 
 def preprocessText(str):
+	t3 = [];
+	L = [];
 	td = {};
-	ts = '';
+	t5 = [];
+
 	t1 = Chunker.chunk(str);
+	st = LancasterStemmer();
 	t2 = SWRemover.removeStopWords(t1);
 	for i in range(len(t2)):
-		if len(t2[i].split()) == 1:				#Checks for NP Chunks
-			t2[i] = re.sub("[0-9]","",t2[i]);
-			p = PorterStemmer.PorterStemmer();
-			t2[i] = p.stem(t2[i],0,len(t2[i])-1);
-		else:		
-			if td.has_key(t2[i]) == False:
-				td[t2[i]] = 1;
-				for j in range(i+1,len(t2)):
-					if t2[i] == t2[j]:
-						td[t2[i]] += 1;
-			t3 = t2[i].split();
-			t4 = SWRemover.removeStopWords(t3);
-			t4 = filter(None, t4);
-			if td[t2[i]] != 1:					#Removes stop words in chunks - Chunk Precision
-				if len(t3) != len(t4):
-					for k in range(len(t4)):
-						ts += t4[k];
-						t2[i] = ts;
-						ts = '';
-			else:								#A chunk is considered valid only if it occurs more than once in a doc, otherwise it is split and stemmed
-				t2.remove(t2[i]);
-				for k in range(len(t4)):
-					t4[k] = p.stem(t4[k],0,len(t4[k])-1);
-				t2 = t2 + t4;
-	t2 = filter(None, t2);				#Removes empty strings
+		t2[i] = re.sub("[0-9]","",t2[i]);
+	t2 = filter(None, t2);
 	for i in range(len(t2)):
-		t2[i] = t2[i].replace(' ','');
-	return t2;
+		t = t2[i].split();
+		if len(t) == 1:
+			L.append(st.stem(t2[i]));
+		else:
+			t3.append(t2[i]);
+	for i in range(len(t3)):
+		if td.has_key(t3[i]) == False:
+			td[t3[i]] = 1;
+			for j in range(i+1,len(t3)):
+				if t3[i] == t3[j]:
+					td[t3[i]] += 1;
+	t4 = td.keys();
+	for i in range(len(t4)):
+		if(td[t4[i]] > 1):
+			for k in range(td[t4[i]]):
+				L.append(t4[i]);
+		else:
+			t = t4[i].split();
+			t5 = t + t5;
+	t5 = SWRemover.removeStopWords(t5);
+	for i in range(len(t5)):
+		t5[i] = st.stem(t5[i]);
+	L = L + t5;
+	for i in range(len(L)):
+		L[i] = L[i].replace(' ','');
+	return L;
